557bf0522d64f34fd428407786d99e47323abd24
Ereg0 <117227719+Ereg0@users.noreply.github.com>
2022-12-14 17:48:49 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+#https://www.youtube.com/watch?v=bfRwxS5d0SI&ab_channel=BroCode
+#w3schools.com
+#https://www.youtube.com/watch?v=M_npdRYD4K0&ab_channel=PatrickLoeber
+#https://www.geeksforgeeks.org/snake-game-in-python-using-pygame-module/
+
+
 # importing libraries
 import pygame
 import random
@@ -15,10 +21,10 @@ white = pygame.Color(255, 0, 0)
 green = pygame.Color(0, 255, 0)
 black = pygame.Color(0, 0, 255)
 
-# Initialising pygame
+# initialise pygame
 pygame.init()
  
-# Initialise game window
+# window around the game, more for decoration
 pygame.display.set_caption('Lil Cuzzys Snakes')
 game_window = pygame.display.set_mode((window_x, window_y))
 
@@ -37,60 +43,52 @@ Snakefood = [random.randrange(1, (window_x//10)) * 10, random.randrange(1, (wind
 #makes food spawn in
 fruit_spawn = True
  
-# setting default snake direction towards
-# right
+# direction snake spawns in
 direction = 'RIGHT'
 change_to = direction
  
-# initial score
+# score
 score = 0
  
-# displaying Score function
+# the score on the top of the game
 def show_score(choice, color, font, size):
-# creating font object score_font
-    score_font = pygame.font.SysFont(font, size)
-     
-    # create the display surface object
-    # score_surface
-    score_surface = score_font.render('Score : ' + str(score), True, color)
-     
-    # create a rectangular object for the text
-    # surface object
-    score_rect = score_surface.get_rect()
+
+# font and design of the score
+    Font = pygame.font.SysFont(font, size)
+    score_backing = Font.render('Score : ' + str(score), True, color)
+    score_rect = score_backing.get_rect()
      
     # displaying text
-    game_window.blit(score_surface, score_rect)
+    game_window.blit(score_backing, score_rect)
  
 # game over function
 def game_over():
    
     # creating font object my_font
-    my_font = pygame.font.SysFont('times new roman', 50)
+    score.font = pygame.font.SysFont('times new roman', 50)
      
     # creating a text surface on which text
     # will be drawn
-    game_over_surface = my_font.render(
+    gameoverback = score.font.render(
         'Your Score is : ' + str(score), True, green)
      
     # create a rectangular object for the text
     # surface object
-    game_over_rect = game_over_surface.get_rect()
+    gameovermiddlesmall = gameoverback.get_rect()
      
     # setting position of the text
-    game_over_rect.midtop = (window_x/2, window_y/4)
+    gameovermiddlesmall.midtop = (window_x/2, window_y/4)
      
     # blit will draw the text on screen
-    game_window.blit(game_over_surface, game_over_rect)
+    game_window.blit(gameoverback, gameovermiddlesmall)
     pygame.display.flip()
      
-    # after 2 seconds we will quit the program
-    time.sleep(2)
-     
+    
     # deactivating pygame library
     pygame.quit()
      
     # quit the program
-    quit()
+   
  
  
 # Main Function
@@ -130,19 +128,13 @@ while True:
     if direction == 'RIGHT':
         Snake[0] += 10
  
-    # Snake body growing mechanism
-    # if fruits and snakes collide then scores
-    # will be incremented by 10
+    # code that makes the snake larger as it eats
     snake_body.insert(0, list(Snake))
     if Snake[0] == Snakefood[0] and Snake[1] == Snakefood[1]:
         score += 10
         fruit_spawn = False
     else:
         snake_body.pop()
-         
-    if not fruit_spawn:
-        Snakefood = [random.randrange(1, (window_x//10)) * 10,
-                          random.randrange(1, (window_y//10)) * 10]
          
     fruit_spawn = True
     game_window.fill(black)
@@ -163,9 +155,6 @@ while True:
     for block in snake_body[1:]:
         if Snake[0] == block[0] and Snake[1] == block[1]:
             game_over()
- 
-    # displaying score countinuously
-    show_score(1, white, 'Press Start 2p', 20)
  
     # Refresh game screen
     pygame.display.update()
